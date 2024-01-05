799c8f9ccb1a84d1333b56b9cf6c53e58c60df59
Priya Kushwaha <99608708+Kushpriya@users.noreply.github.com>
2024-01-05 20:44:30 +0545

    session

--- index.php
+++ index.php
@@ -1,67 +1,13 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-##phpinfo();
+session_start();
 
-##Display
-echo"HEllo World";
-echo"<br>priya";
+//$_SESSION
 
-print"<h1>munni <br>";
-
-
-##concat
-echo"hello" ." " ."munni";
- echo"<br>";
-
-##variables
-$num =1;
-$name = "priya";
-
-echo "my name is $name";
-
-$number =1;
-$floating_number = 1.4;
-$string ="priya";
-$bool =true | false;
-
-echo $floating_number;
-var_dump($number); //vv.imp
+//set session?
+$_SESSION['name']='priya';
 
 
-##function()
-$name_p = "PRIYA";
-function display($name,$age)
-{
-global $name_p;
-echo"<br>i am hungry!!!$name_p";
-}
-
-//display("kush",20);
-
-##conditions
-if(true)
-{
-#do something
-}
-else
-{
-#do something
-}
-
-switch($age)
-{
-case 20:
-#do something
-break;
-default:
-##do something
-}
-
-?>
-
-</body>
-
-</html>
+//get session?
+$name = $_SESSION['name'];
+ var_dump($_SESSION);
+ ?>
